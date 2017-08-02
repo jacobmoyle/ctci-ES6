ca42910aac19e1f062669609a521debba2218d80
Jacob <jacobmoyle@gmail.com>
2017-08-02 02:58:45 -0700

    test, solve, and refactor 1.1

--- app/chapterOne/Ch01_Q01.js
+++ app/chapterOne/Ch01_Q01.js
@@ -1,3 +1,37 @@
-export let Ch01_Q01 = function() {
-  
+//     FIRST ATTEMPT
+// --------------------
+
+// export let Ch01_Q01 = {
+//   submit (string) {
+//     var char_tracker = {};
+//
+//     for (let char of string.split('')) {
+//       char_tracker[char] >= 0 ? char_tracker[char]++ : char_tracker[char] = 0;
+//
+//       if (char_tracker[char] > 1) {
+//         return false;
+//       };
+//
+//     };
+//     return true;
+//   }
+// };
+
+//    Second Attempt
+// --------------------
+
+export let Ch01_Q01 = {
+  submit (string) {
+    var char_tracker = {};
+
+    for (let char of string.split('')) {
+      if (char_tracker[char]) {
+        return false
+      } else {
+        char_tracker[char] = true;
+      };
+
+    };
+    return true;
+  }
 };
--- app/tests/chapterOne/Ch01_Q01.test.js
+++ app/tests/chapterOne/Ch01_Q01.test.js
@@ -1,11 +1,23 @@
 var expect = require('expect');
 
-var {Ch01_Q01} = require('Ch01_Q01');;
+describe('Chapter 1: Arrays and Strings', () => {
 
-describe('Ch01_Q01', () => {
-  it('should exist', () => {
-    expect(Ch01_Q01).toExist();
+  describe('1.1 Is Unique', () => {
+    var {Ch01_Q01} = require('Ch01_Q01');
+
+    it('answer should exist', () => {
+      expect(Ch01_Q01).toExist();
+    });
+
+    it('should return TRUE if a string contains all unique characters', () => {
+      const uniqueString = 'abc'
+      expect(Ch01_Q01.submit(uniqueString)).toBe(true);
+    });
+
+    it('should return FALSE if a string contains all unique characters', () => {
+      const nonUniqueString = 'aaaa'
+      expect(Ch01_Q01.submit(nonUniqueString)).toBe(false);
+    });
   });
 
-  it()
 });
